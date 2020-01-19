3d36b07d2d1a8ab3f037664991c8ba0ae6f7b329
ardmhacha24 <ardmhacha@gmail.com>
2020-01-19 20:10:04 +0000

    creating basic endpoints

--- Rest Endpoint/basic-ctrl-endpoint-SIMPLE.py
+++ Rest Endpoint/basic-ctrl-endpoint-SIMPLE.py
@@ -11,53 +11,67 @@ import subprocess
 class Door(object):
     last_action = None
     last_action_time = None
-    msg_sent = False
-    pb_iden = None
 
     def __init__(self, doorId, config):
         self.id = doorId
         self.name = config['name']
-        self.relay_pin = config['relay_pin']
-        self.state_pin = config['state_pin']
-        self.state_pin_closed_value = config.get('state_pin_closed_value', 0)
-        self.time_to_close = config.get('time_to_close', 10)
-        self.time_to_open = config.get('time_to_open', 10)
+        self.open_pin = config['open_pin']
+        self.close_pin = config['close_pin']
+        self.closed_state_pin = config['closed_state_pin']
+        self.opened_state_pin = config['opened_state_pin']
+        self.pin_closed_value = config.get('pin_closed_value', 0)
+        self.time_to_openclose = config.get('approx_time_to_openclose', 10)
         self.open_time = time.time()
-        gpio.setup(self.relay_pin, gpio.OUT)
-        gpio.setup(self.state_pin, gpio.IN, pull_up_down=gpio.PUD_UP)
-        gpio.output(self.relay_pin, True)
+        #initiatting open/close pins
+        gpio.setup(self.open_pin, gpio.OUT)
+        gpio.output(self.open_pin, True)
+        gpio.setup(self.close_pin, gpio.OUT)
+        gpio.output(self.close_pin, True)
+        #initiating door status reed switches
+        gpio.setup(self.closed_state_pin, gpio.IN, pull_up_down=gpio.PUD_UP)
+        gpio.setup(self.opened_state_pin, gpio.IN, pull_up_down=gpio.PUD_UP)
 
     def get_state(self):
-        if gpio.input(self.state_pin) == self.state_pin_closed_value:
+        if gpio.input(self.closed_state_pin) == self.pin_closed_value:
             return 'closed'
         elif self.last_action == 'open':
-            if time.time() - self.last_action_time >= self.time_to_open:
-                return 'open'
+            if gpio.input(self.opened_state_pin) == self.pin_closed_value:
+                return 'opened'
+            elif (time.time() - self.last_action_time >= self.time_to_openclose) and (gpio.input(self.opened_state_pin) != self.pin_closed_value) and (gpio.input(self.closed_state_pin) != self.pin_closed_value):
+                return 'opening error - taking too long'
             else:
                 return 'opening'
         elif self.last_action ==  'close':
-            if time.time() - self.last_action_time >= self.time_to_close:
-                return 'open' # This state indicates a problem
+            if gpio.input(self.closed_state_pin) == self.pin_closed_value:
+                return 'closed'
+            elif (time.time() - self.last_action_time >= self.time_to_openclose) and (gpio.input(self.closed_state_pin) != self.pin_closed_value) and (gpio.input(self.opened_state_pin) != self.pin_closed_value):
+                return 'closing error - taking too long'
             else:
                 return 'closing'
         else:
-            return 'open'
+            return 'opened'
 
-    def toggle_relay(self):
+    def toggle_relay(self, action):
         state = self.get_state()
-        if (state == 'open'):
-            self.last_action = 'close'
-            self.last_action_time = time.time()
-        elif state == 'closed':
+        if (action == 'open') and (state == 'closed'):
             self.last_action = 'open'
             self.last_action_time = time.time()
+            gpio.output(self.open_pin, False)
+            time.sleep(0.2)
+            gpio.output(self.open_pin, True)
+        elif (action == 'close') and (state == 'opened'):
+            self.last_action = 'close'
+            self.last_action_time = time.time()
+            gpio.output(self.close_pin, False)
+            time.sleep(0.2)
+            gpio.output(self.close_pin, True)
+        elif (state == 'opening') or (state == 'closing'):
+            print('Took no action - already moving... %s:%s:%s', (state, self.last_action,self.last_action_time))
+        elif (state == 'opening error - taking too long') or (state == 'closing error - taking too long'):
+            print('ERROR - investigate as taking too long... %s:%s:%s', (state, self.last_action,self.last_action_time))
         else:
             self.last_action = None
             self.last_action_time = None
-
-        gpio.output(self.relay_pin, False)
-        time.sleep(0.2)
-        gpio.output(self.relay_pin, True)
 
 class Controller(object):
     def __init__(self, config):
@@ -71,7 +85,6 @@ class Controller(object):
         gpio.setmode(gpio.BCM)
         self.config = config
         self.doors = [Door(n, c) for (n, c) in config['doors'].items()]
-        ##self.updateHandler = UpdateHandler(self)
         for door in self.doors:
             door.last_state = 'unknown'
             door.last_state_time = time.time()
@@ -87,14 +100,12 @@ class Controller(object):
                 syslog.syslog('%s: %s => %s' % (door.name, door.last_state, new_state))
                 door.last_state = new_state
                 door.last_state_time = time.time()
-                #self.updateHandler.handle_updates()
 
             if new_state == 'open' and not door.msg_sent and time.time() - door.open_time >= self.ttw:
                 if self.use_alerts:
                     title = "%s's garage door open" % door.name
                     etime = elapsed_time(int(time.time() - door.open_time))
                     message = "%s's garage door has been open for %s" % (door.name, etime)
-                    door.msg_sent = True
 
             if new_state == 'closed':
                 if self.use_alerts:
@@ -103,14 +114,12 @@ class Controller(object):
                         etime = elapsed_time(int(time.time() - door.open_time))
                         message = "%s's garage door is now closed after %s  "% (door.name, etime)
                 door.open_time = time.time()
-                door.msg_sent = False
 
-    def toggle(self, doorId):
+    def toggle(self, doorId, action=None):
         for d in self.doors:
             if d.id == doorId:
                 syslog.syslog('%s: toggled' % d.name)
-                d.toggle_relay()
-                return
+                d.toggle_relay(action)
 
     def get_updates(self, lastupdate):
         updates = []
@@ -125,9 +134,12 @@ class Controller(object):
 
     # check all door status endpoint
     @app.route('/doors', methods=['GET'])
-    def get_doors():
-        return jsonify({'doors': doors})
-        # return jsonify({'doors': [make_public_door(door) for door in doors]})
+    def get_doors_allstatus():
+        for d in self.doors:
+            door_allstat = {
+                d.id : { d.name, d.last_state, d.last_state_time }
+            }
+        return jsonify(door_allstat)
 
     # check individual door status endpoint
     @app.route('/doors/<door>', methods=['GET'])
